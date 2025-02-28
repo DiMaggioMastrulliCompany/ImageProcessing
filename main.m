e60129c0c028e74d4c116063c6bbf53409987a94
val-2 <programming.recantingly@aleeas.com>
2025-02-28 18:43:04 +0100

    immagini come in qgis

--- main.m
+++ main.m
@@ -1,43 +1,56 @@
-% Basic MATLAB script to read and display a TIF file (with potential multiple bands)
-function main()
-% Read the TIF file
+function displayQGISLikeImage()
+% Read the image
 [data, ~] = imread('20240801_ulivo_bandeRED.tif');
 
-% Get information about the data
-[rows, cols, bands] = size(data);
-fprintf('Image dimensions: %d rows x %d columns with %d bands\n', rows, cols, bands);
+% Convert to double for processing
+red = double(data(:,:,1));
+green = double(data(:,:,2));
+blue = double(data(:,:,3));
 
-% Check data type
-fprintf('Data type: %s\n', class(data));
+% Method 1: Use the exact values from QGIS
+% These are the values from your QGIS screenshot
+red_norm1 = (red - 0.0302391) / (0.135319 - 0.0302391);
+green_norm1 = (green - 0.044019) / (0.161973 - 0.044019);
+blue_norm1 = (blue - 0.0365742) / (0.237924 - 0.0365742);
 
-% If multiple bands, display each separately
-if bands > 1
-    for b = 1:bands
-        figure;
-        imagesc(data(:,:,b));
-        colormap gray;
-        colorbar;
-        title(['Band ' num2str(b)]);
-    end
-else
-    % Single band display
-    figure;
-    imagesc(data);
-    colormap gray;
-    colorbar;
-    title('Image Data');
-end
+% Method 2: Calculate values using percentile cutoffs (similar to QGIS's cumulative cut)
+% This excludes outliers by using 2% and 98% percentiles instead of absolute min/max
+red_min2 = prctile(red(:), 2);
+red_max2 = prctile(red(:), 98);
+green_min2 = prctile(green(:), 2);
+green_max2 = prctile(green(:), 98);
+blue_min2 = prctile(blue(:), 2);
+blue_max2 = prctile(blue(:), 98);
 
-% Display histogram to see data distribution
+red_norm2 = (red - red_min2) / (red_max2 - red_min2);
+green_norm2 = (green - green_min2) / (green_max2 - green_min2);
+blue_norm2 = (blue - blue_min2) / (blue_max2 - blue_min2);
+
+% Clip values to 0-1 range for both methods
+red_norm1 = min(max(red_norm1, 0), 1);
+green_norm1 = min(max(green_norm1, 0), 1);
+blue_norm1 = min(max(blue_norm1, 0), 1);
+
+red_norm2 = min(max(red_norm2, 0), 1);
+green_norm2 = min(max(green_norm2, 0), 1);
+blue_norm2 = min(max(blue_norm2, 0), 1);
+
+% Create RGB images
+rgb1 = cat(3, red_norm1, green_norm1, blue_norm1);
+rgb2 = cat(3, red_norm2, green_norm2, blue_norm2);
+
+% Display both versions
 figure;
-if bands > 1
-    for b = 1:bands
-        subplot(bands, 1, b);
-        histogram(double(data(:,:,b)));
-        title(['Histogram for Band ' num2str(b)]);
-    end
-else
-    histogram(double(data));
-    title('Data Histogram');
-end
+subplot(1,2,1);
+imshow(rgb1);
+title('Using QGIS Values');
+
+subplot(1,2,2);
+imshow(rgb2);
+title('Using Percentile Cut (2-98%)');
+
+% Print the calculated percentile values for comparison
+fprintf('Red band - 2%%: %f, 98%%: %f\n', red_min2, red_max2);
+fprintf('Green band - 2%%: %f, 98%%: %f\n', green_min2, green_max2);
+fprintf('Blue band - 2%%: %f, 98%%: %f\n', blue_min2, blue_max2);
 end
