bdb5eaf578f4f4f083fccabfb27736971152faf6
val-2 <programming.recantingly@aleeas.com>
2025-03-13 18:51:28 +0100

    Best one yet

--- main.m
+++ main.m
@@ -20,17 +20,25 @@ ndre = (nir - red_edge) ./ (nir + red_edge);
 vegetation_mask = (ndvi > 0.3) & (ndre > 0.2);
 
 % Apply morphological operations to clean up the mask
-se_small = strel('disk', 3);
-vegetation_mask = imopen(vegetation_mask, se_small);
-vegetation_mask = imclose(vegetation_mask, se_small);
+se_small_1 = strel('disk', 3);
+vegetation_mask_1 = imopen(vegetation_mask, se_small_1);
+vegetation_mask_1 = imclose(vegetation_mask_1, se_small_1);
+
+se_small_2 = strel('disk', 7);
+vegetation_mask_2 = imopen(vegetation_mask_1, se_small_2);
+vegetation_mask_2 = imclose(vegetation_mask_2, se_small_2);
 
 % Rilevazione specifica delle chiome
-se_crown = strel('disk', 20);  % raggio di circa 67 pixel per le chiome
-crown_mask = imopen(vegetation_mask, se_crown);
-crown_mask = imclose(crown_mask, se_crown);
+se_small_3 = strel('disk', 15);
+vegetation_mask_3 = imopen(vegetation_mask_2, se_small_3);
+vegetation_mask_3 = imclose(vegetation_mask_3, se_small_3);
+
+se_small_4 = strel('disk', 25);
+vegetation_mask_4 = imopen(vegetation_mask_3, se_small_4);
+% vegetation_mask_4 = imclose(vegetation_mask_4, se_small_4);
 
 % Label connected components (chiome degli alberi)
-[labeled_trees, num_trees] = bwlabel(crown_mask);
+[labeled_trees, num_trees] = bwlabel(vegetation_mask_4);
 
 % Filter small objects (noise)
 min_tree_size = 10000; % Adjust based on your image resolution
@@ -41,13 +49,13 @@ figure;
 subplot(2,3,1);
 imshow(data(:,:,1:3)); title('RGB Image');
 subplot(2,3,2);
-imshow(ndvi, []); title('NDVI');
+imshow(vegetation_mask, []); title('vegetation_mask');
 subplot(2,3,3);
-imshow(ndre, []); title('NDRE');
+imshow(vegetation_mask_1, []); title('vegetation_mask_1');
 subplot(2,3,4);
-imshow(vegetation_mask, []); title('Vegetation Mask');
+imshow(vegetation_mask_2, []); title('vegetation_mask_2');
 subplot(2,3,5);
-imshow(crown_mask, []); title('Crown Detection');
+imshow(vegetation_mask_3, []); title('vegetation_mask_3');
 subplot(2,3,6);
 imshow(labeled_trees, []); title('Segmented Crowns');
 
