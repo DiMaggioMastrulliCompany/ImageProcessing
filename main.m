b344279269d2e2e0aa8b05091340a831a62fa623
val-2 <programming.recantingly@aleeas.com>
2025-03-21 19:01:51 +0100

    Fine segmentazione

--- main.m
+++ main.m
@@ -16,33 +16,67 @@ ndvi = (nir - red) ./ (nir + red);
 % NDRE (Normalized Difference Red Edge)
 ndre = (nir - red_edge) ./ (nir + red_edge);
 
-% Combine indices for better segmentation
-vegetation_mask = (ndvi > 0.3) & (ndre > 0.2);
+% Convert RGB to HSV for better color discrimination
+% Convert RGB to HSV
+rgb_normalized = cat(3, ...
+    (red - min(red(:))) / (max(red(:)) - min(red(:))), ...
+    (green - min(green(:))) / (max(green(:)) - min(green(:))), ...
+    (blue - min(blue(:))) / (max(blue(:)) - min(blue(:))));
+hsv = rgb2hsv(rgb_normalized);
+saturation = hsv(:,:,2);
+
+% Combine color and vegetation index conditions
+vegetation_mask = (ndre > 0.20) & (saturation < 0.64);
+
+vegetation_mask(1:300, :) = 0;
 
 % Apply morphological operations to clean up the mask
-se = strel('disk', 3);
-vegetation_mask = imopen(vegetation_mask, se);
-vegetation_mask = imclose(vegetation_mask, se);
+se_small_1 = strel('disk', 3);
+vegetation_mask_1 = imopen(vegetation_mask, se_small_1);
+vegetation_mask_1 = imclose(vegetation_mask_1, se_small_1);
 
-% Label connected components (individual trees)
-[labeled_trees, num_trees] = bwlabel(vegetation_mask);
+se_small_2 = strel('disk', 7);
+vegetation_mask_2 = imopen(vegetation_mask_1, se_small_2);
+vegetation_mask_2 = imclose(vegetation_mask_2, se_small_2);
+
+% Rilevazione specifica delle chiome
+se_small_3 = strel('disk', 15);
+vegetation_mask_3 = imopen(vegetation_mask_2, se_small_3);
+vegetation_mask_3 = imclose(vegetation_mask_3, se_small_3);
+
+se_small_4 = strel('disk', 25);
+vegetation_mask_4 = imopen(vegetation_mask_3, se_small_4);
+% vegetation_mask_4 = imclose(vegetation_mask_4, se_small_4);
+
+% Label connected components (chiome degli alberi)
+[labeled_trees, num_trees] = bwlabel(vegetation_mask_4);
 
 % Filter small objects (noise)
-min_tree_size = 13000; % Adjust based on your image resolution
+min_tree_size = 10000; % Adjust based on your image resolution
 labeled_trees = bwareafilt(logical(labeled_trees), [min_tree_size inf]);
 
-% Display results
-figure;
-subplot(2,2,1);
-imshow(data(:,:,1:3)); title('RGB Image');
-subplot(2,2,2);
+% Display results - Figure 1: Input and Indices
+fig1 = figure('Name', 'Input and Vegetation Indices', 'WindowState', 'maximized');
+subplot(2,3,1);
+imshow(cat(3, red, green, blue), []); title('RGB Image');
+subplot(2,3,3);
 imshow(ndvi, []); title('NDVI');
-subplot(2,2,3);
+subplot(2,3,4);
 imshow(ndre, []); title('NDRE');
-subplot(2,2,4);
-imshow(labeled_trees, []); title('Segmented Trees');
+subplot(2,3,5);
+imshow(green, []); title('Green Band');
+subplot(2,3,5);
+imshow(green, []); title('Green Band');
 
-% Optional: Get properties of detected trees
-tree_stats = regionprops(labeled_trees, 'Area', 'Centroid', 'BoundingBox');
-fprintf('Number of detected olive trees: %d\n', length(tree_stats));
-end
+% Display results - Figure 2: Segmentation Steps
+fig2 = figure('Name', 'Segmentation Process', 'WindowState', 'maximized');
+subplot(2,3,1);
+imshow(vegetation_mask, []); title('Initial Mask');
+subplot(2,3,2);
+imshow(vegetation_mask_1, []); title('After 1st Operation');
+subplot(2,3,3);
+imshow(vegetation_mask_2, []); title('After 2nd Operation');
+subplot(2,3,4);
+imshow(vegetation_mask_3, []); title('After 3rd Operation');
+subplot(2,3,5);
+imshow(labeled_trees, []); title('Final Segmentation');
--- ulivo_campo.m
+++ ulivo_campo.m
@@ -28,57 +28,50 @@ wavelengths = info.Wavelength;
 % - NIR (~800-850 nm)
 % - Red Edge (~720-740 nm)
 [~, red_idx] = min(abs(wavelengths - 660));
+[~, green_idx] = min(abs(wavelengths - 550));
+[~, blue_idx] = min(abs(wavelengths - 470));
 [~, nir_idx] = min(abs(wavelengths - 830));
 [~, red_edge_idx] = min(abs(wavelengths - 730));
 
 % Estrai le bande
 red = double(data(:,:,red_idx));
+green = double(data(:,:,blue_idx));
+blue = double(data(:,:,green_idx));
+
 nir = double(data(:,:,nir_idx));
 red_edge = double(data(:,:,red_edge_idx));
 
-% Visualizza un'immagine RGB usando le bande predefinite
-default_bands = info.DefaultBands;  % [20, 13, 6]
-
-% Crea immagine RGB per visualizzazione
-rgb = zeros(info.Height, info.Width, 3);
-for i = 1:3
-    rgb(:,:,i) = data(:,:,default_bands(i));
-end
-rgb_norm = rgb / max(rgb(:));
-
 % Calcola gli indici di vegetazione
-% NDVI (Normalized Difference Vegetation Index)
-ndvi = (nir - red) ./ (nir + red);
-
 % NDRE (Normalized Difference Red Edge)
 ndre = (nir - red_edge) ./ (nir + red_edge);
 
+rgb_normalized = cat(3, ...
+    (red - min(red(:))) / (max(red(:)) - min(red(:))), ...
+    (green - min(green(:))) / (max(green(:)) - min(green(:))), ...
+    (blue - min(blue(:))) / (max(blue(:)) - min(blue(:))));
+hsv = rgb2hsv(rgb_normalized);
+saturation = hsv(:,:,2);
+
+
 % Combina gli indici per una migliore segmentazione
 % Potrebbe essere necessario regolare le soglie in base all'immagine
-vegetation_mask = (ndvi > 0.2) & (ndre > 0.15);
+vegetation_mask = (ndre > 0.15) & (saturation < 0.64);
+
+vegetation_mask(end-10:end, :) = 0;
 
 % Applica operazioni morfologiche per migliorare la maschera
 se_small_1 = strel('disk', 1);
 vegetation_mask_1 = imopen(vegetation_mask, se_small_1);
 vegetation_mask_1 = imclose(vegetation_mask_1, se_small_1);
 
-% se_small_2 = strel('disk', 2);
-% vegetation_mask_2 = imopen(vegetation_mask_1, se_small_2);
-% vegetation_mask_2 = imclose(vegetation_mask_2, se_small_2);
-
-% % Rilevazione specifica delle chiome
-% se_small_3 = strel('disk', 8);
-% vegetation_mask_3 = imopen(vegetation_mask_2, se_small_3);
-% vegetation_mask_3 = imclose(vegetation_mask_3, se_small_3);
-
-% se_small_4 = strel('disk', 12);
-% vegetation_mask_4 = imopen(vegetation_mask_3, se_small_4);
+se_small_2 = strel('disk', 2);
+vegetation_mask_2 = imopen(vegetation_mask_1, se_small_2);
 
 % Etichetta le componenti connesse (chiome degli alberi)
-[labeled_trees, ~] = bwlabel(vegetation_mask_1);
+[labeled_trees, ~] = bwlabel(vegetation_mask_2);
 
 % Filtra gli oggetti piccoli (rumore)
-min_tree_size = 70; % Regola in base alla risoluzione dell'immagine
+min_tree_size = 45; % Regola in base alla risoluzione dell'immagine
 labeled_trees = bwareafilt(logical(labeled_trees), [min_tree_size inf]);
 
 % Visualizza i risultati
@@ -91,22 +84,9 @@ subplot(2,3,3);
 imshow(vegetation_mask_1, []); title('vegetation\_mask\_1');
 subplot(2,3,4);
 imshow(vegetation_mask_2, []); title('vegetation\_mask\_2');
-subplot(2,3,5);
-imshow(vegetation_mask_3, []); title('vegetation\_mask\_3');
 subplot(2,3,6);
 imshow(labeled_trees, []); title('Segmented Crowns');
 
 % Calcola le proprietà degli alberi rilevati
 tree_stats = regionprops(labeled_trees, 'Area', 'Centroid', 'BoundingBox');
 fprintf('Numero di alberi di ulivo rilevati: %d\n', length(tree_stats));
-
-% Visualizza i centroidi degli alberi sull'immagine RGB
-figure;
-imshow(rgb_norm);
-hold on;
-for i = 1:length(tree_stats)
-    centroid = tree_stats(i).Centroid;
-    plot(centroid(1), centroid(2), 'r+', 'MarkerSize', 10, 'LineWidth', 2);
-end
-title('Alberi di ulivo identificati');
-hold off;
